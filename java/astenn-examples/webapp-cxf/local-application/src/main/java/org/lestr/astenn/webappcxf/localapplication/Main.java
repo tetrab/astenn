d8c4a20a456e6e5ca1201c98fb95516f66f29b67
pierreantoine.bonnin@gmail.com <pierreantoine.bonnin@gmail.com@2c2b4fe6-f4fb-c69f-8173-c589a82545f2>
2011-10-24 14:24:10 +0000

    [maven-release-plugin]  copy for tag astenn-1.0.1

---
+++ java/astenn-examples/webapp-cxf/local-application/src/main/java/org/lestr/astenn/webappcxf/localapplication/Main.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2011 pibonnin.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.lestr.astenn.webappcxf.localapplication;
+
+import org.lestr.astenn.PluginsManager;
+import org.lestr.astenn.webappcxf.api.IPlugin;
+
+/**
+ *
+ * @author pibonnin
+ */
+public class Main {
+
+
+    public static void main(String... args) {
+
+        PluginsManager.getSingleton().registerPlugin(IPlugin.class, LocalPlugin.class);
+        PluginsManager.getSingleton().registerPlugin(IPlugin.class, "soap:http://localhost:8080/remote-application-1.0-SNAPSHOT/org.lestr.astenn.webappcxf.api.IPlugin/org.lestr.astenn.webappcxf.remoteapplication.RemotePlugin");
+
+        for(IPlugin plugin : PluginsManager.getSingleton().getRegisteredPlugins(IPlugin.class))
+            System.out.println(plugin.sayHello());
+
+    }// END Method main
+
+
+}// END Class Main
